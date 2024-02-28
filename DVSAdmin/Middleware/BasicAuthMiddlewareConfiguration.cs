6e7afd0b9a44ee66a0bf30743073b89406061f70
Vishal Vishwanathan <vishal.vishwanathan@beis.gov.uk>
2024-02-28 14:10:15 +0000

    feat(basicauth): Added Basic Auth Middleware for dev, staging phase

---
+++ DVSAdmin/Middleware/BasicAuthMiddlewareConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DVSAdmin.Middleware
+{
+	public class BasicAuthMiddlewareConfiguration
+	{
+        public const string ConfigSection = "BasicAuth";
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
