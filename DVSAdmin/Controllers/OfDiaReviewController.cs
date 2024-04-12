67c1a31d43ceaaeaeb38523d8cd061e664dbad8b
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-04-12 10:39:42 +0100

    feat(ofdia-review): add controller and view

---
+++ DVSAdmin/Controllers/OfDiaReviewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DVSAdmin.Controllers
+{
+    [Route("")]
+    public class OfDiaReviewController : Controller
+    {
+        [Route("")]
+        [HttpGet("start-page")]
+        public IActionResult StartPage()
+        {
+            return View();
+        }
+
+
+        [HttpGet("preregistration-review")]
+        public IActionResult PreRegistrationReview()
+        {
+            return View();
+        }
+
+
+       
+    }
+}
--- DVSAdmin/Program.cs
+++ DVSAdmin/Program.cs
@@ -45,9 +45,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
 
