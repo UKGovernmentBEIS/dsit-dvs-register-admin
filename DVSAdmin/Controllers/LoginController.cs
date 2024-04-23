56e314fc8578e7599a4b502c0c088822c1dd2bde
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-04-23 10:04:52 +0100

    feat(pre-reg-review): login controller and views

---
+++ DVSAdmin/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DVSAdmin.Controllers
+{
+    [Route("")]
+    public class LoginController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewAccount()
+        {
+            return RedirectToAction("EnterEmailAddress", "Login");
+        }
+
+        [HttpGet("enter-email-address")]
+        public IActionResult EnterEmailAddress()
+        {
+            return View("EnterEmailAddress");
+        }
+
+        //TODO: Add Sign in Action
+    }
+}
