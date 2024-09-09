4f365a10c2c6b15b1b488b737a7934744300969a
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-09-09 14:08:47 +0100

    fix(pi-check): folder structure corrected, removed unused references

--- DVSAdmin/Controllers/PublicInterestCheckController.cs
+++ DVSAdmin/Controllers/PublicInterestCheckController.cs
@@ -1,37 +1,29 @@
-﻿using DVSAdmin.BusinessLogic.Models;
-using DVSAdmin.BusinessLogic.Services;
-using DVSAdmin.CommonUtility.Models.Enums;
+﻿using DVSAdmin.BusinessLogic.Services;
 using DVSAdmin.Models;
-using DVSRegister.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DVSAdmin.Controllers
 {
 
     [Route("public-interest-check")]
-    //[ValidCognitoToken]
+    [ValidCognitoToken]
     public class PublicInterestCheckController : Controller
     {
-        private readonly ILogger<PublicInterestCheckController> logger;
+      
         private readonly IPublicInterestCheckService publicInterestCheckService;
-        private readonly IUserService userService;
-        public PublicInterestCheckController(ILogger<PublicInterestCheckController> logger, IPublicInterestCheckService publicInterestCheckService, IUserService userService)
-        {
-            this.logger = logger;
-            this.publicInterestCheckService = publicInterestCheckService;
-            this.userService = userService;
+       
+        public PublicInterestCheckController(IPublicInterestCheckService publicInterestCheckService)
+        {           
+            this.publicInterestCheckService = publicInterestCheckService;            
         }
 
         [HttpGet("public-interest-check-list")]
         public async Task<IActionResult> PublicInterestCheck()
         {
         
-                PublicInterestCheckViewModel publicInterestCheckViewModel = new PublicInterestCheckViewModel();
-
-                var publicinterestchecks = await publicInterestCheckService.GetPICheckList();
-
-                return View(publicInterestCheckViewModel);
-           
+        PublicInterestCheckViewModel publicInterestCheckViewModel = new PublicInterestCheckViewModel();
+        var publicinterestchecks = await publicInterestCheckService.GetPICheckList();
+        return View(publicInterestCheckViewModel);          
 
         }
     }
