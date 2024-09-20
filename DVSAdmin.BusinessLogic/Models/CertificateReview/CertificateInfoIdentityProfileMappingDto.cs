661ea1aa50f15d25913469cc9a719529923bb4eb
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-09-20 12:43:42 +0100

    fix(code-clean-up): removed unused classes

--- DVSAdmin.BusinessLogic/Models/CertificateReview/CertificateInfoIdentityProfileMappingDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DVSAdmin.BusinessLogic.Models
-{
-    public class CertificateInfoIdentityProfileMappingDto
-    {
-        public int Id { get; set; }       
-        public int CertificateInformationId { get; set; }
-        public int IdentityProfileId { get; set; }        
-    }
-}
