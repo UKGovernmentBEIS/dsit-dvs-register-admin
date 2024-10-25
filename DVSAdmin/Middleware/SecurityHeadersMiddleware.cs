298f7471923efcf11393c894318217f9f29f8353
Naghma Khan <naghma.khan@ie.ey.com>
2024-10-25 11:14:32 +0100

    fix(dvs-45):added ga url's for img-src csp header.

--- DVSAdmin/Middleware/SecurityHeadersMiddleware.cs
+++ DVSAdmin/Middleware/SecurityHeadersMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Headers;
 using System.Text;
 using DVSAdmin.CommonUtility;
+using Humanizer.Localisation;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Extensions.Options;
 
@@ -9,26 +10,25 @@ namespace DVSAdmin.Middleware
     public class SecurityHeadersMiddleware
     {
         private readonly RequestDelegate _next;
+        private const string sources = "https://www.google-analytics.com https://ssl.google-analytics.com https://www.googletagmanager.com https://www.region1.google-analytics.com https://region1.google-analytics.com; ";
         public SecurityHeadersMiddleware(RequestDelegate next)
         {
             _next = next;//Storing the reference to next middleware in pipeline
         }
+
         public async Task InvokeAsync(HttpContext context)
         {
             if (!context.Response.HasStarted)
             {
                 // Added security headers
                 context.Response.Headers["X-Frame-Options"] = "DENY";
-                context.Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate, private";
-                context.Response.Headers["Pragma"] = "no-cache";
-                context.Response.Headers["Expires"] = "-1";
 
-                //CSP for inline scripts
+                //CSP with nonce for inline scripts
                 context.Response.Headers["Content-Security-Policy"] =
-                "script-src 'unsafe-inline' 'self' https://www.google-analytics.com https://ssl.google-analytics.com https://www.googletagmanager.com https://www.region1.google-analytics.com https://region1.google-analytics.com; " +
+                "script-src 'unsafe-inline' 'self' " + sources +
                 "object-src 'none'; " +
-                "connect-src 'self' https://www.google-analytics.com https://ssl.google-analytics.com https://www.googletagmanager.com https://www.region1.google-analytics.com https://region1.google-analytics.com; " +
-                "img-src 'self'; " +
+                "connect-src 'self' " + sources +
+                "img-src 'self' " + sources +
                 "style-src 'self'; " +
                 "base-uri 'self'; " +
                 "font-src 'self'; " +
