3abfdf6aeefc9a3de7b4129f227fdcfb9dedec5b
Aiswarya Rajendran <Aiswarya.Rajendran@ie.ey.com>
2025-02-10 17:37:47 +0000

    fix(code-clean): remove unused method

--- DVSAdmin.Data/Repositories/RegisterManagement/IRegManagementRepository.cs
+++ DVSAdmin.Data/Repositories/RegisterManagement/IRegManagementRepository.cs
@@ -13,9 +13,7 @@ namespace DVSAdmin.Data.Repositories
         public Task<ProviderProfile> GetProviderWithServiceDetails(int providerId);
         public Task<GenericResponse> UpdateServiceStatus(List<int> serviceIds, int providerId, ServiceStatusEnum serviceStatus, string loggedInUserEmail);
         public Task<GenericResponse> UpdateProviderStatus(int providerId, ProviderStatusEnum providerStatus,string loggedInUserEmail);
-        public Task<GenericResponse> SavePublishRegisterLog(RegisterPublishLog registerPublishLog, string loggedInUserEmail);
-        public  Task<GenericResponse> UpdateRemovalStatus(EventTypeEnum eventType, TeamEnum team, int providerProfileId,
-        List<int> serviceIds, string loggedInUserEmail, RemovalReasonsEnum? reason, ServiceRemovalReasonEnum? serviceRemovalReason); // to do remove after merge
+        public Task<GenericResponse> SavePublishRegisterLog(RegisterPublishLog registerPublishLog, string loggedInUserEmail);       
         public Task<List<Service>> GetPublishedServices();
     }
 }
