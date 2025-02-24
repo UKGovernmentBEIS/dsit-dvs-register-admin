15bb0157a0fc58bda1a43c35b04aa5212ee00f05
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-02-24 14:10:05 +0000

    feat(provider-edit): edit repository with save provider draft method

---
+++ DVSAdmin.Data/Edit/IEditRepository.cs
@@ -0,0 +1,10 @@
+﻿using DVSAdmin.CommonUtility.Models;
+using DVSAdmin.Data.Entities;
+
+namespace DVSAdmin.Data.Edit
+{
+    public interface IEditRepository
+    {
+        public Task<GenericResponse> SaveProviderDraft(ProviderProfileDraft draft, string loggedInUserEmail);
+    }
+}
