1d3990706317b51272da66b715bb3daddc69e2f8
JoeGLauria <129402373+JoeGLauria@users.noreply.github.com>
2025-05-15 16:30:47 +0100

    feat(cab-transfer) : add the service list of services that can be reassigned cabs

---
+++ DVSAdmin.BusinessLogic/Services/CabTransfer/ICabTransferService.cs
@@ -0,0 +1,10 @@
+﻿using DVSAdmin.BusinessLogic.Models;
+using static DVSAdmin.BusinessLogic.Services.CabTransfer.CabTransferService;
+
+namespace DVSAdmin.BusinessLogic.Services
+{
+    public interface ICabTransferService
+    {
+        public Task<PaginatedResult<ServiceDto>> GetServices(int pageNumber);
+    }
+}
---
+++ DVSAdmin.Data/Repositories/CabTransfer/CabTransferRepository.cs
@@ -0,0 +1,60 @@
+﻿using DVSAdmin.CommonUtility.Models;
+using DVSAdmin.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace DVSAdmin.Data.Repositories
+{
+    public class CabTransferRepository: ICabTransferRepository
+    {
+        private readonly DVSAdminDbContext context;
+        private readonly ILogger<CabTransferRepository> logger;
+
+        public CabTransferRepository(DVSAdminDbContext context, ILogger<CabTransferRepository> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public class PaginatedResult<T>
+        {
+            public List<T> Items { get; set; }
+            public int TotalCount { get; set; }
+        }
+
+        public async Task<PaginatedResult<Service>> GetServices(int pageNumber)
+        {
+            var baseQuery = context.Service
+                .Include(s => s.Provider)
+                .Include(s => s.CertificateReview)
+                .Include(s => s.ServiceRoleMapping)
+                .Include(s => s.CabUser).ThenInclude(s => s.Cab)
+                .Where(s => s.ServiceStatus == ServiceStatusEnum.Published || s.ServiceStatus == ServiceStatusEnum.Removed);
+
+            var groupedQuery = await baseQuery
+                .GroupBy(s => s.ServiceKey)
+                .Select(g => g.OrderByDescending(s => s.ServiceVersion).FirstOrDefault())
+                .ToListAsync();
+
+            var orderedQuery = groupedQuery
+                .OrderBy(s => s.ServiceStatus)
+                .ThenBy(s => s.ModifiedTime); 
+
+            var totalCount = orderedQuery.Count();
+
+            var items = orderedQuery
+                .Skip((pageNumber - 1) * 10)
+                .Take(10)
+                .ToList(); 
+
+            return new PaginatedResult<Service>
+            {
+                Items = items,
+                TotalCount = totalCount
+            };
+
+        }
+
+    }
+}
