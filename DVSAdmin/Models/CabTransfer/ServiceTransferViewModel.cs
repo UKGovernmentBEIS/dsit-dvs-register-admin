0b11d50cb629ab10392205147fe313cd796c718b
JoeGLauria <129402373+JoeGLauria@users.noreply.github.com>
2025-05-20 18:15:58 +0100

    feat(cab-transfer) : add service details page and change the query to include new staus

---
+++ DVSAdmin/Models/CabTransfer/ServiceTransferViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DVSAdmin.BusinessLogic.Models;
+using DVSAdmin.CommonUtility.Models.Enums;
+
+namespace DVSAdmin.Models
+{
+    public class ServiceTransferViewModel
+    {
+        public ServiceDto Service { get; set; }
+        public bool IsServiceDetailsPage { get; set; }
+        public string? ToCabName { get; set; }
+    }
+}
