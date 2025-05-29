e1b0a1827439af85e39a53134d5b81faeaa449c1
Kobithasan Vasantharajah <kobithasan.vasantharajah@uk.ey.com>
2025-05-29 09:29:28 +0100

    fix(cab-transfer): CabTransfer controller and view refactoring

--- DVSAdmin/Models/CabTransfer/ConfirmCabAcceptanceViewModel.cs
+++ DVSAdmin/Models/CabTransfer/ConfirmCabAcceptanceViewModel.cs
@@ -1,15 +1,14 @@
 ﻿namespace DVSAdmin.Models.CabTransfer
 {
-    public class SubmissionCheckViewModel
+    public class ConfirmCabAcceptanceViewModel
     {
         public int    ServiceId   { get; set; }
         public string ServiceName { get; set; }
-
         public int    CurrentCabId   { get; set; }
         public string CurrentCabName { get; set; }
-
         public int    SelectedCabId   { get; set; }
         public string SelectedCabName { get; set; }
+        public ServiceTransferViewModel TransferViewModel { get; set; }
     }
 }
 
