331707109932fb4346d062907298947ab537e8f3
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-07-03 16:31:15 +0100

    fix(cab): fix to display only cabs with active services

---
+++ DVSAdmin/Models/ViewModelHelper.cs
@@ -0,0 +1,18 @@
+﻿using DVSAdmin.BusinessLogic.Models;
+using DVSAdmin.CommonUtility.Models;
+
+namespace DVSAdmin.Models
+{
+    public class ViewModelHelper
+    {
+        public static List<string> GetCabsForProvider(List<ServiceDto> services)
+        {
+            return services .Where(s => s.ServiceStatus == ServiceStatusEnum.ReadyToPublish ||
+                    s.ServiceStatus == ServiceStatusEnum.Published ||
+                    s.ServiceStatus == ServiceStatusEnum.PublishedUnderReassign ||
+                    s.ServiceStatus == ServiceStatusEnum.CabAwaitingRemovalConfirmation ||
+                    s.ServiceStatus == ServiceStatusEnum.UpdatesRequested)
+                   .Select(s => s.CabUser.Cab.CabName).Distinct().ToList();
+        }
+    }
+}
