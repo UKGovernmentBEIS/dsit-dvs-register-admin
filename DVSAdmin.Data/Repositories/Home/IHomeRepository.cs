c8ebc5e4810844ea5d884023d1185d919b1d2551
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-08-26 16:36:57 +0100

    fix(admin): pagination and count query refactoring, submitted displayed as received

--- DVSAdmin.Data/Repositories/Home/IHomeRepository.cs
+++ DVSAdmin.Data/Repositories/Home/IHomeRepository.cs
@@ -1,11 +1,14 @@
 ﻿using DVSAdmin.Data.Entities;
-using static DVSAdmin.Data.Repositories.CabTransferRepository;
+using DVSAdmin.Data.Models;
 
 namespace DVSAdmin.Data.Repositories
 {
     public interface IHomeRepository
     {
-        public Task<PaginatedResult<Service>> GetServices(string loggedInUserEmail,int pageNumber, string sort, string sortAction, string openTask);
+        public Task<PaginatedResult<Service>> GetPendingCertificateReviews(int pageNumber, string sort, string sortAction);
+        public Task<PaginatedResult<Service>> GetPendingPrimaryChecks(string loggedInUserEmail, int pageNumber, string sort, string sortAction);
+        public Task<PaginatedResult<Service>> GetPendingSecondaryChecks(string loggedInUserEmail, int pageNumber, string sort, string sortAction);
+        public Task<PaginatedResult<Service>> GetPendingRequests(string loggedInUserEmail, int pageNumber, string sort, string sortAction);     
         public Task<Dictionary<string, int>> GetPendingCounts(string loggedInUserEmail);
         public Task<User> GetUserByEmail(string userEmail);
     }
